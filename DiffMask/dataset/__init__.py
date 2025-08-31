4a9826484869a3125acbf4b25e0c3b6d929a3008
Pakawat Phasook <pakawat.phas@kmutt.ac.th>
2025-09-01 03:20:12 +0700

    update loading data emidec

--- DiffMask/dataset/__init__.py
+++ DiffMask/dataset/__init__.py
@@ -1,3 +1,4 @@
 from dataset.lidc_mask import LIDCMASKDataset
 from dataset.lidc_mask_in import LIDCMASKInDataset
+from dataset.emidec_mask import EMIDECMASKDataset
 
--- DiffMask/get_dataset/get_dataset.py
+++ DiffMask/get_dataset/get_dataset.py
@@ -1,5 +1,5 @@
 from torch.utils.data import DataLoader
-from dataset import LIDCMASKDataset, LIDCMASKInDataset
+from dataset import LIDCMASKDataset, LIDCMASKInDataset, EMIDECMASKDataset
 
 def get_inference_dataloader(dataset_root_dir, test_txt_path, batch_size=1, drop_last=False):
     train_dataset = LIDCMASKInDataset(root_dir=dataset_root_dir, test_txt_path=test_txt_path)
@@ -8,7 +8,11 @@ def get_inference_dataloader(dataset_root_dir, test_txt_path, batch_size=1, drop
 
 
 def get_train_dataset(cfg):
-    train_dataset = LIDCMASKDataset(root_dir=cfg.dataset.root_dir, text_txt_path=cfg.dataset.test_txt_dir)
+    # Select appropriate dataset class based on dataset name
+    if cfg.dataset.name == 'emidec_mask':
+        train_dataset = EMIDECMASKDataset(root_dir=cfg.dataset.root_dir, text_txt_path=cfg.dataset.test_txt_dir)
+    else:
+        train_dataset = LIDCMASKDataset(root_dir=cfg.dataset.root_dir, text_txt_path=cfg.dataset.test_txt_dir)
     sampler = None
     return train_dataset, sampler
 
